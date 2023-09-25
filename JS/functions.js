8868edb5064babadcacc42ad721559b16956776e
Smeet <smeetagrawal2003@gmail.com>
2023-09-25 21:59:18 +0530

    All files in folder JS

---
+++ JS/functions.js
@@ -0,0 +1,14 @@
+function print(name)
+{
+    console.log(name)
+}
+
+let name0 = "Smeet"
+let name1 = "Smeet1"
+let name2 = "Smeet2"
+let name3 = "Smeet23"
+let name4 = "Smeet3";
+
+print(name0)
+print(name1)
+print(name2)
